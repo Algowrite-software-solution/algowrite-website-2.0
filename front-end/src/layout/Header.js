d8cb0e7a2284a12ee6e365f20a03a7922b2fef34
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-06-15 01:40:02 +0530

    colors added

--- front-end/src/layout/Header.js
+++ front-end/src/layout/Header.js
@@ -2,11 +2,14 @@ import React from "react";
 import Button from "../components/Button";
 
 function Header() {
-   return(
-     <div>
-          <Button className="text-amber-600 bg-red-300 w-24 h-14" text="Nisal"/>
-     </div>
-   );  
+  return (
+    <div>
+      <Button
+        className="font-bold bg-primary text-white py-2 px-5 rounded-full"
+        text="Nisal"
+      />
+    </div>
+  );
 }
 
 export default Header;
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: "#0F5AEC",
+        secondary: "#25B0FF",
+        darker: "#000A6B",
+        dark: "#1324BE",
+        light: "#8CD5FF",
+        white: "#F6FCFF",
+      },
+      colors: {
+        primary: "#0F5AEC",
+        secondary: "#25B0FF",
+        darker: "#000A6B",
+        dark: "#1324BE",
+        light: "#8CD5FF",
+        white: "#F6FCFF",
+      },
+      borderColor: {
+        primary: "#0F5AEC",
+        secondary: "#25B0FF",
+        darker: "#000A6B",
+        dark: "#1324BE",
+        light: "#8CD5FF",
+        white: "#F6FCFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
