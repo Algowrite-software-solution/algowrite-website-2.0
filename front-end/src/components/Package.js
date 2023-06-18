3afa3abc813e2abf92586908870f6c5a94249850
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-06-18 17:01:09 +0530

    packages added

--- front-end/src/components/Package.js
+++ front-end/src/components/Package.js
@@ -1,36 +1,76 @@
 // import image from "../assets/PackageImages/Rectangle_23.png";
 
-function Packages({image,package_name,content}) {
-    return (
-        <div>
-            <div className="rounded-[30px] shadow-2xl shadow-blue-950 w-60 min-h-[360px] bg-secondary group">
-                <div className="group-hover:hidden ">
-                    <img src={image} />
-                    <div className="pt-3 mx-2 max-h-[4px]">
-                        <h1 className="text-center text-white font-[800] text-[22px] tracking-[1.6px]">{package_name}</h1>
-                        <span className="text-white font-[500] text-[13px]">{content}</span><br />
-                        <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                        <span className="text-white font-[500] text-[13px]">Package Name Packge</span>
-                    </div>
-                </div>
-                <div className="pt-3 mx-2 max-h-[4px] hidden group-hover:block">
-                    <h1 className="text-center text-white font-[800] text-[22px] tracking-[1.6px]">Package Name</h1>
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                    <span className="text-white font-[500] text-[13px]">Package Name Packge</span><br />
-                </div>
-            </div>
-
+function Packages({ image, package_name, content }) {
+  return (
+    <div className="flex justify-center">
+      <div className="rounded-[30px] my-20 shadow-2xl shadow-blue-950 w-60 min-h-[360px] bg-secondary group">
+        <div className="group-hover:hidden ">
+          <img src={image} />
+          <div className="pt-3 mx-2 max-h-[4px]">
+            <h1 className="text-center text-white font-[800] text-[22px] tracking-[1.6px]">
+              {package_name}
+            </h1>
+            <span className="text-white font-[500] text-[13px]">{content}</span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+          </div>
         </div>
-    )
+        <div className="absolute">
+          {/* <div className="pt-3 mx-2 max-h-[4px] hidden group-hover:block">
+            <h1 className="text-center text-white font-[800] text-[22px] tracking-[1.6px]">
+              Package Name
+            </h1>
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+            <span className="text-white font-[500] text-[13px]">
+              Package Name Packge
+            </span>
+            <br />
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Packages;
-
--- front-end/src/layout/PackageBox.js
+++ front-end/src/layout/PackageBox.js
@@ -1,5 +1,6 @@
 import Packages from "../components/Package";
 import image from "../assets/PackageImages/Rectangle_23.png";
+import MainTopic from "../components/MainTopic";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -7,96 +8,96 @@ import "swiper/css/pagination";
 
 // import "./styles.css";
 
-import { Pagination } from "swiper";
+import { Keyboard, Navigation, Pagination, Virtual } from "swiper";
 
 function PackageBox() {
-    const packages = [
-        {
-            id: 1,
-            image: image,
-            package_name: "Packge Name 1",
-            content: "vgrhttrhyt"
-        },
-        {
-            id: 2,
-            image: image,
-            package_name: "Packge Name 2",
-            content: "vgrhttrhyt"
-        },
-        {
-            id: 3,
-            image: image,
-            package_name: "Packge Name 3",
-            content: "vgrhttrhyt"
-        },
-        {
-            id: 4,
-            image: image,
-            package_name: "Packge Name 4",
-            content: "vgrhttrhyt"
-        },
-        {
-            id: 5,
-            image: image,
-            package_name: "Packge Name 5",
-            content: "vgrhttrhyt"
-        },
-    ];
+  const packages = [
+    {
+      id: 1,
+      image: image,
+      package_name: "Packge Name 1",
+      content: "vgrhttrhyt",
+    },
+    {
+      id: 2,
+      image: image,
+      package_name: "Packge Name 2",
+      content: "vgrhttrhyt",
+    },
+    {
+      id: 3,
+      image: image,
+      package_name: "Packge Name 3",
+      content: "vgrhttrhyt",
+    },
+    {
+      id: 4,
+      image: image,
+      package_name: "Packge Name 4",
+      content: "vgrhttrhyt",
+    },
+    {
+      id: 5,
+      image: image,
+      package_name: "Packge Name 5",
+      content: "vgrhttrhyt",
+    },
+  ];
 
-    return (
-        <div>
-            <div className="bg-primary min-h-[510px] md:pt-3 pb-7">
-                <h3 className="text-center text-[36px] text-white font-[900] leading-10">Packages</h3>
-                <p className="text-center text-white font-[700]">Tag line of services</p>
-
-
-                <Swiper
-                    slidesPerView={4}
-                    spaceBetween={10}
-                    pagination={{
-                        className: 'page',
-                        clickable: true,
-                    }}
-                    breakpoints={{
-                        "@0.00": {
-                            slidesPerView: 1,
-                            spaceBetween: 10,
-                        },
-                        "@0.75": {
-                            slidesPerView: 2,
-                            spaceBetween: 20,
-                        },
-                        "@1.00": {
-                            slidesPerView: 3,
-                            spaceBetween: 40,
-                        },
-                        "@1.50": {
-                            slidesPerView: 4,
-                            spaceBetween: 50,
-                        },
-                    }}
-                    modules={[Pagination]}
-                    className="mySwiper"
-                >
-                    <div className="flex flex-wrap content-center justify-around">
-                       <div>
-                       {packages.map((item) => (
-                            <SwiperSlide key={item.id}>
-                                <Packages
-                                    image={item.image}
-                                    package_name={item.package_name}
-                                    content={item.content}
-                                />
-                            </SwiperSlide>
-                        ))}
-                       </div>
-                    </div>
-                </Swiper>
-            </div>
+  return (
+    <section className="bg-primary px-5">
+      <div className="py-4  mx-auto px-10 xl:w-[1200px] flex justify-center flex-col">
+        <MainTopic text="Packages" subTopic="this is the sub topic" />
+        <div className=" flex items-center">
+          <Swiper
+            autoplay={true}
+            pagination={{
+              className: "page",
+              clickable: true,
+            }}
+            navigation={{
+              enabled: true,
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            breakpoints={{
+              "@0.50": {
+                slidesPerView: 1,
+                spaceBetween: 10,
+              },
+              "@1.00": {
+                slidesPerView: 2,
+                spaceBetween: 2,
+              },
+              "@1.25": {
+                slidesPerView: 3,
+                spaceBetween: 2,
+              },
+              "@1.75": {
+                slidesPerView: 4,
+                spaceBetween: 2,
+              },
+            }}
+            modules={[Pagination, Navigation]}
+            className="mySwiper mx-12 "
+          >
+            {packages.map((item) => (
+              <SwiperSlide
+                key={item.id}
+                className="flex justify-center items-center"
+              >
+                <Packages
+                  image={item.image}
+                  package_name={item.package_name}
+                  content={item.content}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
-    
+      </div>
+    </section>
   );
 }
 
 export default PackageBox;
-
