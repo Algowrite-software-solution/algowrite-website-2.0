c55d176883fb80dcf552c588d5ba2fc9e8c2a8a5
MADUSHA151 <105775877+MADUSHA151@users.noreply.github.com>
2023-06-19 15:30:20 +0530

    technology slider basic structure
    technology slider basic structure completed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 // import Button from './components/Button';
 import MainTopic from './components/MainTopic';
 import Header from './layout/Header';
+import Technology from './layout/Technology';
 
 function App() {
   return (
     <div>
-      <MainTopic className="text-red-500" text="SERVICE" subTopic="Enter you subTopic" subTopicClassName="text-blue-600" />
-      <Header />
+      <Technology />
     </div>
   );
 }
