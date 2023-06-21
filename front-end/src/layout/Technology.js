5f08590e83d071e4048b5d5b61f7e5bece576600
kavindu202 <kavindu.sasanka962@gmail.com>
2023-06-21 16:20:54 +0530

    edferfg

--- front-end/src/layout/Technology.js
+++ front-end/src/layout/Technology.js
@@ -10,7 +10,7 @@ import javaScript from "../assets/TechnologyIcons/javaScript.png";
 
 
 // Import Swiper React components
-import { Swiper, SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
@@ -20,7 +20,7 @@ import "swiper/css/navigation";
 import "./TechnologyStyles.css";
 
 // import required modules
-import { Pagination, Navigation , Autoplay } from "swiper";
+import { Pagination, Navigation, Autoplay } from "swiper";
 import MainTopic from "../components/MainTopic";
 
 
@@ -28,49 +28,51 @@ function Technology() {
 
      return (
 
-          <div className="py-7 w-full h-1/5">
-               <div className="flex justify-center text-center pb-10">
-                    <MainTopic text="TECHNOLOGIES" className="text-darker font-bold text-3xl" subTopic="Tag line of technology" subTopicClassName="text-darker" />
+          <div className="w-full py-7 h-1/5">
+               <div className="flex justify-center pb-10 text-center">
+                    <MainTopic text="TECHNOLOGIES" className="text-3xl font-bold text-darker" subTopic="Tag line of technology" subTopicClassName="text-darker" />
                </div>
 
-               <div>
-                    <Swiper
-                         slidesPerView={1}
-                         spaceBetween={10}
-                         pagination={{
-                              clickable: true,
-                         }}
-                         navigation={true}
-                         autoplay={{
-                              delay: 2500,
-                              disableOnInteraction: false,
-                         }}
-                         
-                         breakpoints={{
-                              640: {
-                                   slidesPerView: 2,
-                                   spaceBetween: 20,
-                              },
-                              768: {
-                                   slidesPerView: 4,
-                                   spaceBetween: 40,
-                              },
-                              1024: {
-                                   slidesPerView: 5,
-                                   spaceBetween: 50,
-                              },
-                         }}
-                         modules={[Pagination, Navigation ,Autoplay]}
-                         className="mySwiper"
-                    >
-                         <SwiperSlide ><img src={react} /></SwiperSlide>
-                         <SwiperSlide ><img src={javaScript} /></SwiperSlide>
-                         <SwiperSlide ><img src={java} /></SwiperSlide>
-                         <SwiperSlide ><img src={mysql} /></SwiperSlide>
-                         <SwiperSlide ><img src={nodejs} /></SwiperSlide>
-                         <SwiperSlide ><img src={php} /></SwiperSlide>
-                         <SwiperSlide ><img src={bootstrap} /></SwiperSlide>
-                    </Swiper>
+               <div className="ml-20 mr-20 bg-red-60">
+                    <div className="">
+                         <Swiper
+                              slidesPerView={1}
+                              spaceBetween={10}
+                              pagination={{
+                                   clickable: true,
+                              }}
+                              navigation={true}
+                              autoplay={{
+                                   delay: 2500,
+                                   disableOnInteraction: false,
+                              }}
+
+                              breakpoints={{
+                                   640: {
+                                        slidesPerView: 2,
+                                        spaceBetween: 20,
+                                   },
+                                   768: {
+                                        slidesPerView: 4,
+                                        spaceBetween: 40,
+                                   },
+                                   1024: {
+                                        slidesPerView: 5,
+                                        spaceBetween: 50,
+                                   },
+                              }}
+                              modules={[Pagination, Navigation, Autoplay]}
+                              className="mySwiper"
+                         >
+                              <SwiperSlide ><img src={react} /></SwiperSlide>
+                              <SwiperSlide ><img src={javaScript} /></SwiperSlide>
+                              <SwiperSlide ><img src={java} /></SwiperSlide>
+                              <SwiperSlide ><img src={mysql} /></SwiperSlide>
+                              <SwiperSlide ><img src={nodejs} /></SwiperSlide>
+                              <SwiperSlide ><img src={php} /></SwiperSlide>
+                              <SwiperSlide ><img src={bootstrap} /></SwiperSlide>
+                         </Swiper>
+                    </div>
                </div>
           </div>
      );
